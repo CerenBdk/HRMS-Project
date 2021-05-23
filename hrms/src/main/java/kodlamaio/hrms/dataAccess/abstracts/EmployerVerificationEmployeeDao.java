d42487b037c1107702a3543fba7a6ed33e5dfeaf
CerenBdk <cerenbdk@gmail.com>
2021-05-23 16:48:59 +0300

    bug fix

--- hrms/src/main/java/kodlamaio/hrms/dataAccess/abstracts/EmployerVerificationEmployeeDao.java
+++
@@ -1,9 +0,0 @@
-package kodlamaio.hrms.dataAccess.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import kodlamaio.hrms.entities.concretes.EmployerVerificationEmployee;
-
-public interface EmployerVerificationEmployeeDao extends JpaRepository<EmployerVerificationEmployee, Integer>{
-
-}
