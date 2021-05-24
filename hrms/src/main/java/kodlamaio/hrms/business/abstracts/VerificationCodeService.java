3df8ea6a5bb8e8df44c0af323b2bf39c792c75d0
CerenBdk <cerenbdk@gmail.com>
2021-05-24 22:09:18 +0300

    VerificationCodeService and VerificationCodeManager have been added.

---
+++ hrms/src/main/java/kodlamaio/hrms/business/abstracts/VerificationCodeService.java
@@ -0,0 +1,18 @@
+package kodlamaio.hrms.business.abstracts;
+
+import java.util.List;
+
+import kodlamaio.hrms.core.utilities.results.DataResult;
+import kodlamaio.hrms.core.utilities.results.Result;
+
+import kodlamaio.hrms.entities.concretes.VerificationCode;
+
+public interface VerificationCodeService {
+
+	Result add(VerificationCode code);
+	Result delete(VerificationCode code);
+	Result update(VerificationCode code);
+	
+    DataResult<VerificationCode> getById(int id);
+	DataResult<List<VerificationCode>> getAll();
+}
---
+++ hrms/src/main/java/kodlamaio/hrms/business/concretes/VerifictionCodeManager.java
@@ -0,0 +1,55 @@
+package kodlamaio.hrms.business.concretes;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kodlamaio.hrms.business.abstracts.VerificationCodeService;
+import kodlamaio.hrms.core.utilities.results.DataResult;
+import kodlamaio.hrms.core.utilities.results.Result;
+import kodlamaio.hrms.core.utilities.results.SuccessDataResult;
+import kodlamaio.hrms.core.utilities.results.SuccessResult;
+import kodlamaio.hrms.dataAccess.abstracts.VerificationCodeDao;
+import kodlamaio.hrms.entities.concretes.VerificationCode;
+
+@Service
+public class VerifictionCodeManager implements VerificationCodeService{
+
+	private VerificationCodeDao verificationCodeDao;
+	
+	@Autowired
+	public VerifictionCodeManager(VerificationCodeDao verificationCodeDao) {
+		super();
+		this.verificationCodeDao = verificationCodeDao;
+	}
+
+	@Override
+	public Result add(VerificationCode code) {
+		this.verificationCodeDao.save(code);
+		return new SuccessResult("Code has been saved.");
+	}
+
+	@Override
+	public Result delete(VerificationCode code) {
+		this.verificationCodeDao.delete(code);
+		return new SuccessResult("Code has been deleted.");
+	}
+
+	@Override
+	public Result update(VerificationCode code) {
+		this.verificationCodeDao.save(code);
+		return new SuccessResult("Code has been updated.");
+	}
+
+	@Override
+	public DataResult<VerificationCode> getById(int id) {
+		return new SuccessDataResult<VerificationCode>(this.verificationCodeDao.getOne(id));
+	}
+
+	@Override
+	public DataResult<List<VerificationCode>> getAll() {
+		return new SuccessDataResult<List<VerificationCode>>(this.verificationCodeDao.findAll());
+	}
+
+}
