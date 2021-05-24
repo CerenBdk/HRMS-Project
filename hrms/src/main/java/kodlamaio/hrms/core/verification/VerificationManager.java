8af8fcc16216dfb5b7b5539f684593c5211574c5
CerenBdk <cerenbdk@gmail.com>
2021-05-24 17:36:42 +0300

    sendLink and sendCode methods have been added for email verification.

---
+++ hrms/src/main/java/kodlamaio/hrms/core/verification/VerificationManager.java
@@ -0,0 +1,25 @@
+package kodlamaio.hrms.core.verification;
+
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+@Service
+public class VerificationManager implements VerificationService{
+
+	@Override
+	public void sendLink(String email) {
+		UUID uuid = UUID.randomUUID();
+		String verificationLink = "https://hrmsverificationmail/" + uuid.toString();
+		System.out.println("Verification link has been sent to " + email );
+		System.out.println("Please click on the link to verify your account:  " + verificationLink );
+	}
+
+	@Override
+	public void sendCode(String email) {
+		UUID uuid = UUID.randomUUID();
+		String verificationCode = uuid.toString();
+		System.out.println("Verification link has been sent to " + email );
+		System.out.println("Your activation code:  " + verificationCode );
+	}
+
+}
---
+++ hrms/src/main/java/kodlamaio/hrms/core/verification/VerificationService.java
@@ -0,0 +1,6 @@
+package kodlamaio.hrms.core.verification;
+
+public interface VerificationService {
+	void sendLink(String email);
+	void sendCode(String email);
+}
