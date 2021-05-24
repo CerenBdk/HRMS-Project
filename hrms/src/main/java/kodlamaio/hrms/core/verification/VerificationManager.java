20f73f683ca0c5a2157575e1e2e1121e918c8eaa
CerenBdk <cerenbdk@gmail.com>
2021-05-24 21:35:35 +0300

    sendCode method has been updated.

--- hrms/src/main/java/kodlamaio/hrms/core/verification/VerificationManager.java
+++ hrms/src/main/java/kodlamaio/hrms/core/verification/VerificationManager.java
@@ -1,10 +1,23 @@
 package kodlamaio.hrms.core.verification;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import kodlamaio.hrms.dataAccess.abstracts.VerificationCodeDao;
+import kodlamaio.hrms.entities.concretes.VerificationCode;
 @Service
 public class VerificationManager implements VerificationService{
+
+	private VerificationCodeDao verificationCodeDao;
+	
+	@Autowired
+	public VerificationManager(VerificationCodeDao verificationCodeDao) {
+		super();
+		this.verificationCodeDao = verificationCodeDao;
+	}
 
 	@Override
 	public void sendLink(String email) {
@@ -15,11 +28,12 @@ public class VerificationManager implements VerificationService{
 	}
 
 	@Override
-	public void sendCode(String email) {
+	public void sendCode(String email, int id) {
 		UUID uuid = UUID.randomUUID();
 		String verificationCode = uuid.toString();
+		VerificationCode code = new VerificationCode(id, verificationCode, false, LocalDate.now());
+		this.verificationCodeDao.save(code);
 		System.out.println("Verification link has been sent to " + email );
 		System.out.println("Your activation code:  " + verificationCode );
 	}
-
 }
